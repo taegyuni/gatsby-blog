bbc57bea1b837c02cd553b14ff00109cf4613d76
taegyuni <taegyun7553@naver.com>
2021-09-03 01:40:42 +0900

    start blog!

---
+++ gatsby-meta-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: `Slow but Fast`,
+  description: `Blog posted about ...`,
+  author: `Taegyuni`,
+  introduction: `언제나 화이팅!`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `taegyuni`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: `taegyun.dev/`, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'tgkim',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
